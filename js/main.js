c2132e56c910c77aa06ccc2ed09ade377bb76498
dokoede <dennisokoede@outlook.com>
2019-09-24 11:22:10 +0100

    added js validation to signin

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 /* js file for signup/ login page */
 new WOW().init();
+
+const signin = document.querySelector(".form-signin"),
+  feedback = document.querySelector(".feedback");
+
+signin.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const username = signin.username.value;
+  //   validation using regex
+  const usernamePattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (usernamePattern.test(username)) {
+    // feedback success
+    feedback.textContent = "Email Valid";
+  } else {
+    feedback.textContent = "Email Invalid";
+  }
+});
