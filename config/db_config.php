c2e731ad179efb0091fd6ebe2ef15883a0d25cce
lanrayjay <54362209+lanrayjay@users.noreply.github.com>
2019-10-02 21:34:31 +0100

    backend config file

---
+++ config/db_config.php
@@ -0,0 +1,16 @@
+<?php
+    /*
+ * All database connection variables
+ */
+//offline config details
+define("DB_USER", "olayiwola"); // db user
+define("DB_PASSWORD", "olayiwola"); // db password (mention your db password here)
+define("DB_DATABASE", "hera"); // database name
+define("DB_HOST", "localhost"); // db server
+
+//online config details
+//define("DB_USER", "findakit_hera"); // db user
+//define("DB_PASSWORD", "TeamHera123"); // db password (mention your db password here)
+//define("DB_DATABASE", "findakit_hera"); // database name
+//define("DB_HOST", "localhost"); // db server
+?>
